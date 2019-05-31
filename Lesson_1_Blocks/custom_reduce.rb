f0babb0766b489e6a8c7556f2d67b26c7ce9c59d
Eden Eltume <IEltume@gmail.com>
2019-05-31 07:27:00 -0400

    Lesson 1: Blocks

---
+++ Lesson_1_Blocks/custom_reduce.rb
@@ -0,0 +1,19 @@
+def reduce(array, default = 0)
+  counter = 0
+  acc = default
+  
+  while counter < array.size
+    current_element = array[counter]
+    acc = yield(acc, current_element)
+    counter += 1
+  end
+
+  acc
+end
+
+
+array = [1, 2, 3, 4, 5]
+
+p reduce(array) { |acc, num| acc + num }                    # => 15
+p reduce(array, 10) { |acc, num| acc + num }                # => 25
+#p reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
