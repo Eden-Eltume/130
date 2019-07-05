335a3b0e4416b657848c0bdd48347ac4be2cea1d
Eden Eltume <IEltume@gmail.com>
2019-07-04 22:58:12 -0400

    Easy 1 Ex. 6-10

---
+++ 130_Exercises/1_Easy 1/07_iterators:_true_for_all?.rb
@@ -0,0 +1,20 @@
+def all?(arr)
+  bool_arr = []
+  arr.each do |num|
+    if yield(num)
+      bool_arr.push(true)
+    else
+      bool_arr.push(false)
+    end
+  end
+
+  bool_arr.include?(false) ? false : true
+end
+
+p all?([1, 3, 5, 6]) { |value| value.odd? } == false
+p all?([1, 3, 5, 7]) { |value| value.odd? } == true
+p all?([2, 4, 6, 8]) { |value| value.even? } == true
+p all?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+p all?([1, 3, 5, 7]) { |value| true } == true
+p all?([1, 3, 5, 7]) { |value| false } == false
+p all?([]) { |value| false } == true
---
+++ 130_Exercises/1_Easy 1/09_iterators:_true_for_one?.rb
@@ -0,0 +1,20 @@
+def one?(arr)
+  bool_arr = []
+  arr.each do |num|
+    if yield(num)
+      bool_arr.push(true)
+    else
+      bool_arr.push(false)
+    end
+  end
+
+  bool_arr.count(true) == 1 ? true : false
+end
+
+p one?([1, 3, 5, 6]) { |value| value.even? }    # -> true
+p one?([1, 3, 5, 7]) { |value| value.odd? }     # -> false
+p one?([2, 4, 6, 8]) { |value| value.even? }    # -> false
+p one?([1, 3, 5, 7]) { |value| value % 5 == 0 } # -> true
+p one?([1, 3, 5, 7]) { |value| true }           # -> false
+p one?([1, 3, 5, 7]) { |value| false }          # -> false
+p one?([]) { |value| true }                     # -> false
