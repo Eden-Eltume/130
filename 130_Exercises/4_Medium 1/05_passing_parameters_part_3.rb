3acb2daddac62febd287488637acb801b1fae890
Eden Eltume <IEltume@gmail.com>
2019-07-09 18:46:28 -0400

    Medium 1 Ex. 5-7

---
+++ 130_Exercises/4_Medium 1/05_passing_parameters_part_3.rb
@@ -0,0 +1,34 @@
+items = ['apples','corn','cabbage','wheat']
+
+def gather(items)
+  puts "Let's start gathering food."
+  yield(items)
+  puts "We've finished gathering!"
+end
+
+#1
+gather(items) do |*produce, wheat|
+  puts produce.join(", ")
+  puts wheat
+end
+
+puts
+#2
+gather(items) do |apples, *vegetables, wheat|
+  puts apples
+  puts vegetables.join(", ")
+  puts wheat
+end
+
+puts
+#3
+gather(items) do |apples, *assorted|
+  puts apples
+  puts assorted.join(', ')
+end
+
+puts
+#4
+gather(items) do |apples, corn, cabbage, wheat|
+  puts "#{apples}, #{corn}, #{cabbage}, #{wheat}"
+end
